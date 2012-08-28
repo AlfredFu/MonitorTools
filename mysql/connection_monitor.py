0fb5d355d102af2b7a129ae1177312f49d69e105
Fred Fu <fred.fu@lexisnexis.com>
2012-08-28 15:27:19 +0800

    monitor tool for mysql connection

---
+++ mysql/connection_monitor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+import MySQLdb
+import sys
+import lexismail
+#os.system("mmm_control show")
+db_conn=MySQLdb.connect("host","user",'password','databasename')
+db_cursor=db_conn.cursor()
+db_cursor.execute("show processlist")
+
+conncount=db_cursor.rowcount
+#print conncount
+if conncount>50:
+        lexismail.sendNotification("Too many connection on p05,connections now:%d" % conncount)
+        #os.system("mmm_control set_offline db1");
+        pass
