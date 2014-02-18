b6dc6e2aa3057d8a912fb75c3ffbe619ca725d7f
Fred Fu <mingzhou87@gmail.com>
2014-02-18 11:09:17 +0800

    Update kill_processlist.php

--- mysql/kill_processlist.php
+++ mysql/kill_processlist.php
@@ -4,7 +4,7 @@ $result = mysql_query("SHOW FULL PROCESSLIST");
 
 while ($row=mysql_fetch_array($result)) {
     $process_id=$row["Id"];
-    if ($row['Host'] !='ip-42-2-168-192.rev.dyxnet.com:28662' && $row['User']!='system user' && $row['User']!='replication') {
+    if ($row['Host'] !='hostname' && $row['User']!='system user' && $row['User']!='replication') {
         $sql="KILL $process_id";
         mysql_query($sql);
     }   
